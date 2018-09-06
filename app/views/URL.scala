fcff45e380d18dc8d5ea0afd22c26de7ab8ca5d7
IWAMOTO Kouichi <sue@iwmt.org>
2018-09-06 21:51:34 +0900

    myfleet.moe を確保できたので URL を元に戻す。
    myfleet.iwmt.org -> myfleet.moe

--- app/views/URL.scala
+++ app/views/URL.scala
@@ -6,7 +6,7 @@ package views
  * Date: 15/02/09.
  */
 object URL {
-  val Host = "https://myfleet.iwmt.org"
+  val Host = "https://myfleet.moe"
   def Index = Host
   val Entire = s"${Host}/entire"
   val Statistics = s"${Entire}/activities"
